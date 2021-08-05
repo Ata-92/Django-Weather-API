b1768318434903e50d1d6606ec6ad7d4fda39d10
Ata-92 <atakal92@gmail.com>
2021-08-05 20:10:44 +0300

    Get data and send it to template

--- src/weather_app/views.py
+++ src/weather_app/views.py
@@ -3,14 +3,35 @@ from decouple import config
 import requests
 from pprint import pprint
 
+from weather_app.models import City
+
 # Create your views here.
 
 def home_view(request):
+    cities = City.objects.all()
     url = config("BASE_URL")
-    city = "Berlin"
-    r = requests.get(url.format(city))
+    # city = "Berlin"
+    # r = requests.get(url.format(city))
     # "https://api.openweathermap.org/data/2.5/weather?q={}&appid=480221af134ea47c38f04b9244c0f45a".format("Berlin")
-    context = r.json()
-    pprint(context)
+    # context = r.json()
+    # pprint(context)
 
-    return render(request, "weather_app/home.html")
+    city_data = []
+    for city in cities:
+        print(city)
+        r = requests.get(url.format(city))
+        content = r.json()
+        pprint(content)
+        data = {
+            "city": city.name,
+            "temperature": content["main"]["temp"],
+            "description": content["weather"][0]["description"],
+            "icon": content["weather"][0]["icon"]
+        }
+        city_data.append(data)
+
+    print(city_data)
+    context = {
+        "city_data": city_data
+    }
+    return render(request, "weather_app/home.html", context)
