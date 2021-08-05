6948e66ef698e16415d3829f22a9070d2da391d1
Ata-92 <atakal92@gmail.com>
2021-08-05 18:32:54 +0300

    Get data via api

--- src/weather_app/views.py
+++ src/weather_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
 # Create your views here.
 
 def home_view(request):
+    url = config("BASE_URL")
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    # "https://api.openweathermap.org/data/2.5/weather?q={}&appid=480221af134ea47c38f04b9244c0f45a".format("Berlin")
+    context = r.json()
+    pprint(context)
+
     return render(request, "weather_app/home.html")
